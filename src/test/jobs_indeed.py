ba568662ceff5240d693c6ec7a72ea62955b37f3
charles <charles.logeais@yahoo.fr>
2025-03-20 17:06:09 +0100

    delete test

--- src/test/jobs_indeed.py
+++
@@ -1,4 +0,0 @@
-import hrequests
-
-response = hrequests.get('https://www.apec.fr/candidat/recherche-emploi.html/emploi?motsCles=data%20scientist&lieux=596212&typesConvention=143684&typesConvention=143685&typesConvention=143686&typesConvention=143687&typesContrat=101888&page=0&distance=10')
-print(response.text)
